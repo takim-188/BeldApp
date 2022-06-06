75592890cec9e6ea39daaf99120f015a57e4e113
Beyza Gonculer <beyza.gonculer1@gmail.com>
2022-06-06 23:23:04 +0300

    hatalar düzeltildi

--- lib/gelen_bildiriler.dart
+++ lib/gelen_bildiriler.dart
@@ -2,12 +2,12 @@ import 'package:beldapp/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+
 
 class BildiriGelen extends StatefulWidget {
 
 
- BildiriGelen({Key? key, required this.data}) : super(key: key);
+  BildiriGelen({Key? key, required this.data}) : super(key: key);
 
   var data;
   @override
@@ -23,76 +23,81 @@ class _BildiriGelenState extends State<BildiriGelen> {
 
   var info;
 
-  @override
-  void initState() {
-    super.initState();
-    getImage();
-  }
+ 
 
 
-  getImage()async{
-    var data = FirebaseFirestore.instance.collection("Bildiriler").snapshots();
-  }
+
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        toolbarHeight: 70,
-        title: Text("Gelen Bildiriler", style: TextStyle(color: Colors.black),),
-        actions: [
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Text(DateFormat('yyyy-mm-dd \n KK:mm:ss').format(DateTime.now()), style: TextStyle(color: Colors.black),),
+        appBar: AppBar(
+          title: Text('Gelen Bildiriler',style:TextStyle(
+              color:Colors.white,
+              fontWeight:FontWeight.w400,
+              fontFamily:"OpenSans",
+              fontSize: 18),),
+          toolbarHeight: 80,
+          backgroundColor:Color(0xfff20c60),
+          shape: RoundedRectangleBorder(
+
+              borderRadius:  BorderRadius.only(
+
+                  bottomRight: Radius.circular(70),
+
+                  bottomLeft: Radius.circular(70))
+
           ),
-        ],
-      ),
-      body: Container(
-        child: StreamBuilder<QuerySnapshot>(
-          stream: widget.data,
-          builder: (context, snapshot){
-            if(snapshot.hasError){
-              return Text("something went wrong");
-            }
-            if(snapshot.connectionState == ConnectionState.waiting){
-              return Center(
-                child: SizedBox(
-                    height: 30,
-                    width: 30,
-                    child: CircularProgressIndicator()
-                ),
-              );
-            }
-            else{
-              final data = snapshot.requireData;
-              return ListView.builder(
-                itemCount: currentItem.length,
-                itemBuilder: (context, index){
-                  var description = data.docs[index]["description"];
-                  var image = data.docs[index]["image"];
-                  var title = data.docs[index]["title"];
-                  var type = data.docs[index]["type"];
-                  var user = data.docs[index]["user"];
-                  return ListTile(
-                    trailing: Text(title),
-                    leading: CircleAvatar(
-                      radius: 28,
-                      backgroundImage: NetworkImage(image),
-                    ),
-                    title: Text(type, style: TextStyle(color: Colors.black),),
-                    subtitle: Text(description),
-                  );
-                },
-
-              );
-            }
-
-          },
+          elevation: 14,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Menu',
+            onPressed: () {},
+          ), //IconButton
         ),
-      )
+        body: Container(
+          child: StreamBuilder<QuerySnapshot>(
+            stream: widget.data,
+            builder: (context, snapshot){
+              if(snapshot.hasError){
+                return Text("Bir şeyler yanlış gidiyor");
+              }
+              if(snapshot.connectionState == ConnectionState.waiting){
+                return Center(
+                  child: SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: CircularProgressIndicator()
+                  ),
+                );
+              }
+              else{
+                final data = snapshot.requireData;
+                return ListView.builder(
+                  itemCount: currentItem.length,
+                  itemBuilder: (context, index){
+                    var description = data.docs[index]["description"];
+                    var image = data.docs[index]["image"];
+                    var title = data.docs[index]["title"];
+                    var type = data.docs[index]["type"];
+                    return ListTile(
+                      trailing: Text(type),
+                      leading: CircleAvatar(
+                        radius: 28,
+                        backgroundImage: NetworkImage(image),
+                      ),
+                      title: Text(title, style: TextStyle(color: Colors.black),),
+                      subtitle: Text(description),
+                    );
+                  },
+
+                );
+              }
+
+            },
+          ),
+        )
 
 
     );
@@ -105,74 +110,46 @@ class itemList {
   static final getData = [
 
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
     {
-      'title': '',
+      'title': 'jhjknk',
       'image':
       'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
-    },
-    {
-      'title': '',
-      'image':
-      'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
-    },
-    {
-      'title': '',
-      'image':
-      'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
-    },
-    {
-      'title': '',
-      'image':
-      'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
-    },
-    {
-      'title': '',
-      'image':
-      'https://migros-dali-storage-prod.global.ssl.fastly.net/sanalmarket/product/11010010/11010010-1ae231-1650x1650.jpg',
-      'description': '',
-      'type': '',
+      'description': 'lmlşö',
+      'type': 'jklmlk',
     },
 
 
